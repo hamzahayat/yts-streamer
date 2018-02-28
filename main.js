6afd0531e19a187daf49c66f1f0d62bd7b9f6691
Hamza Qaisrani <hamza.qaisrani.hq@gmail.com>
2018-02-28 11:08:55 -0500

    Fixed bugs and refactored code

--- main.js
+++ main.js
@@ -18,54 +18,15 @@ function createWindow(){
 
     //Navigate To index.html
     mainWindow.loadURL(url.format({
-        pathname: path.join(__dirname, 'src/index.html'),
+        pathname: path.join(__dirname, '/index.html'),
         protocol: 'file:',
         slashes: true
     }))
-    
-    //Open Developer Console 
-    mainWindow.webContents.openDevTools("detach")
 
     //Set Window variable to Null on close
     mainWindow.on('closed', () => {
         mainWindow = null
     })
 }
-
-
-
-// ipcMain.on('openFile', (event, path) => {
-
-//     //Declare Variable Constants
-//     const {dialog} = require('electron');
-//     const fs = require('fs');
-
-//     //Show Open Dialog Box
-//     dialog.showOpenDialog(function(fileNames){
-       
-//         //If No File Selected
-//         if (fileNames === undefined){
-//             console.log("No File Selected");
-//         } else{
-//             readFile(fileNames[0]);
-//         }
-//     });
-
-//     //Read File Function
-//     function readFile(filepath){
-//         fs.readFile(filepath, 'utf-8', (err, data) => {
-            
-//             if (err){
-//                 alert("An Error ocurred reading the file :" + err.message);
-//                 return
-//             }
-
-//             //Handle the file content
-//             event.sender.send('fileData', data);
-
-//         });
-//     }
-
-// });
 
 app.on('ready', createWindow);
